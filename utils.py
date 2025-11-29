47b8a3946513dff852cab2b68731f8f234b1ee32
kelvinperez <kelvin_klncs@hotmail.com>
2025-11-29 18:55:28 -0300

    atualizacao utils

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
-header = "a versao deste arquivo e a numero 3"
+header = "a versao deste arquivo e a numero 2"
+
+class testes:
+    def __init__(self, nome: str, idade: int, altura: int):
+        self.nome = nome 
+        self.idade = idade
+        self.altura = altura 
+        
+    def meunome(self):
+        return print(self.nome)
+
+
+kelvin = testes('kelvin', 30, 173) 
+
+kelvin.meunome()
