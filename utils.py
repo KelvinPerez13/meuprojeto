450395840bca213674352e059ff8ddfcc48effd4
kelvinperez <kelvin_klncs@hotmail.com>
2025-11-29 23:04:17 -0300

    ajustes commit

--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
-<<<<<<< HEAD
-header = "a versao deste arquivo e a numero 2"
-=======
-header = "a versao deste arquivo e a numero 3"
->>>>>>> nova_branch
+
+class testes:
+    def __init__(self, nome: str, idade: int, altura: int):
+        self.nome = nome 
+        self.idade = idade
+        self.altura = altura 
+        
+    def meunome(self):
+        return print(self.nome)
+
+    def idade(self):
+        return print(self.idade)
+
+kelvin = testes('kelvin', 30, 173) 
+
+kelvin.meunome()
